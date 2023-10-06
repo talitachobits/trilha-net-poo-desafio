4dbf4f32224f77ffbfdc60c0da1f4a72fdee1522
talitachobits <talitachobits@gmail.com>
2023-10-05 23:52:33 -0300

    Adicionado o código proposto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("11987871313", "Nokia tijolo", "fsfdsfdsf", 64);
+Iphone iphone = new Iphone("11956764323", "Iphone de botão", "879kyt", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
